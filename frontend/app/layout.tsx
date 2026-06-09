aba023cbb2ad0785a2db6420f0b050e6ea6d8c18
LucasRomano <lucasivanromano@gmail.com>
2026-06-09 15:59:35 +1000

    opne graph

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,30 @@ import ClientLayout from "@/components/ClientLayout";
 export const metadata: Metadata = {
   title: "Wheely Good",
   description: "Bike rentals and shop in Byron Bay",
+
+  metadataBase: new URL("https://www.wheelygoodrides.com.au"),
+
+  openGraph: {
+    title: "Wheely Good",
+    description: "Bike rentals and shop in Byron Bay",
+    url: "https://www.wheelygoodrides.com.au",
+    siteName: "Wheely Good",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Wheely Good",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Wheely Good",
+    description: "Bike rentals and shop in Byron Bay",
+    images: ["/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({
